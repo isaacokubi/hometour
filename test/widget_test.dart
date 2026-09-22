d911c3779b8e7977b7a3603d74de629df9d12d9d
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-22 23:23:11 +0300

    feat: complete Firebase migration for test/widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hometour/main.dart';
 
 void main() {
-  testWidgets('Global Tours app renders its initial loading state', (tester) async {
+  testWidgets('Global Tours app renders its loading state', (tester) async {
     await tester.pumpWidget(const HomeTourApp(autoBootstrap: false));
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
