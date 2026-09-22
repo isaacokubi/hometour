763e3be4a849ac8e29652c528201267f8565c1ea
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-22 20:25:58 +0300

    test: stabilize Flutter app smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hometour/main.dart';
 
 void main() {
-  testWidgets('Global Tours app starts', (tester) async {
+  testWidgets('Global Tours app renders its initial loading state', (tester) async {
     await tester.pumpWidget(const HomeTourApp());
-    expect(find.text('Global Tours'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 }
